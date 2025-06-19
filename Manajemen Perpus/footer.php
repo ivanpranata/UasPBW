229ad8a9efca0e6927907157735a7298a2f4365f
ivanpranata <van3saudara@gmail.com>
2025-06-19 16:00:33 +0700

    Hapus file Awal

--- Manajemen Perpus/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col-xs-12" align="center">
-            Copyright &COPY; <?= date('Y')?> Kelompok RPL | INFORMATIKA
-        </div>
-    </div>
-</div>
